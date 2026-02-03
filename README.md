cedfe5b437fe873051535abc0d3eae15aaf91438
马金青 <837335472@qq.com>
2026-02-03 16:48:55 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue 3 + TypeScript + Vite
+
+这个模板应该能帮助你开始使用 Vue 3 和 TypeScript 在 Vite 中进行开发。
+该模板使用 Vue 3 的 `<script setup>` 单文件组件，
+查看 [script setup 文档](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) 
+了解更多信息。
+
+在 [Vue 文档 TypeScript 指南](https://vuejs.org/guide/typescript/overview.html#project-setup) 
+中了解更多关于推荐的项目设置和 IDE 支持。
---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+let num = 0;
+const unusedVar = "This variable is not used";
+console.log(undeclaredVar); // undeclaredVar 未声明
+</script>
+
+<template>
+  <div>
+    <!-- element plus组件 -->
+    <el-button type="primary">测试element plus</el-button>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo"/>
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue"/>
+  <el-button type="primary">测试element plus</el-button>
+  <div class="demo-box">
+    <div class="tips">111111</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+/* 测试scss代码 */
+.demo-box {
+  background-color: $theme-color;
+  width: $main-width;
+  height: $main-height;
+
+  .tips {
+    color: red;
+  }
+}
+</style>
